f6a1a3993b23b7815527e3edec8c3c5d1ccd4b80
dark-ether <henrique.hsa2002@gmail.com>
2023-02-16 10:09:00 -0300

    modified haskelll template

--- flakes/nixpkgs-haskell/flake.nix
+++ flakes/nixpkgs-haskell/flake.nix
@@ -5,9 +5,11 @@
     nixpkgs.url = "nixpkgs";
     flake-parts.url = "github:hercules-ci/flake-parts";
     treefmt-nix.url = "github:numtide/treefmt-nix";
+    haskell-flake.url = "github:srid/haskell-flake";
+    mission-control.url = "github:Platonic-Systems/mission-control";
   };
 
-  outputs = inputs@{ flake-parts, nixpkgs, treefmt-nix,... }:
+  outputs = inputs@{ flake-parts, nixpkgs, treefmt-nix,mission-control, haskell-flake,... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
         # To import a flake module
@@ -15,15 +17,27 @@
         # 2. Add foo as a parameter to the outputs function
         # 3. Add here: foo.flakeModule
         treefmt-nix.flakeModule
+        mission-control.flakeModule
+        haskell-flake.flakeModule
       ];
       systems = [ "x86_64-linux" "aarch64-darwin" ];
       perSystem = { config, self', inputs', pkgs, system, ... }: {
         # Per-system attributes can be defined here. The self' and inputs'
         # module parameters provide easy access to attributes of the same
         # system.
-
+        haskellProjects.main = {
+          packages.PLACEHOLDER.root = ./.;
+          devShell = {
+            enable = true;
+            # add shellHook when merged
+          };
+        };
         # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
-
+        treefmt.programs = {
+          cabal-fmt.enable = true;
+          hlint.enable = true;
+        };
+        devShells.default = config.devShells.main;
       };
       flake = {
         # The usual flake attributes can be defined here, including system-
